e5f73b91244c2fe3f2e8bc46b6ab8d03a2656050
TimeLordRu <timelordru@bk.ru>
2023-06-05 10:25:54 +0700

    accedit.php

--- accedit.php
+++ accedit.php
@@ -1,10 +1,34 @@
-<title>Настройка аккаунта</title>
-<script src="https://kit.fontawesome.com/aac53a065a.js" crossorigin="anonymous"></script>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<title>Таблица аккаунтов</title>
+<head>
+    <link type="image/x-icon" href="http://localhost/favicon.ico" rel="shortcut icon">
+    <link type="Image/x-icon" href="http://localhost/favicon.ico" rel="icon">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <style type="text/css">
+        TABLE {
+            width: 600px; /* Ширина таблицы */
+            border-collapse: collapse; /* Убираем двойные линии между ячейками */
+        }
+        TD, TH {
+            padding: 3px; /* Поля вокруг содержимого таблицы */
+            border: 1px solid black; /* Параметры рамки */
+        }
+        TH {
+            background: #b0e0e6; /* Цвет фона */
+        }
+
+    </style>
+</head>
+
 <link rel="stylesheet" href="css1.css">
- <header>
+<script language="javascript" type="text/javascript">
+    <a href="#" onclick="window.close();">Закрыть вкладку</a>;
+    </script>
+  <header>
   <nav class="links">
     <a href="index.php">На главную</a>
-<a href="acctable.php">Назад</a>
+
     <a href="logout.php">Выход</a>
     
   </nav>
@@ -12,126 +36,84 @@
 <br><br>
 <?php
 session_start();
-require_once 'setting.php';
-//echo '<i class="fa-solid fa-thumbs-up fa-2x"></i>';
-$connection = new mysqli($host, $user, $pass, $data);
-if ($connection->connect_error) die('Error connection');
-$id = $_GET['id'];
-$query = "SELECT * FROM users WHERE id = $id";
-$result = mysqli_query($connection, $query);
+echo "<center>";
+    require_once 'setting.php';
+    $connection = new mysqli($host, $user, $pass, $data);
+    if ($connection->connect_error) die('Error connection');
+    if ($_SESSION['role'] != 0) {
+//получение кол-ва записей
+        $sql = "SELECT COUNT(*) as total FROM users";
+        $result = $connection->query($sql);
+        $row = $result->fetch_assoc();
+        $amount = $row['total'];
+        echo "<table>";
 
-if (mysqli_num_rows($result) == 1) {
-    $row = mysqli_fetch_assoc($result);
-    $log = $row['login'];
-$role1 = $row['role'];
-    $pass = $row['passw'];
-    $algorithm = "sha256";
-    //$pass = hash($algorithm, $pass);
-    if ($row['role'] == 0) $role = "Заблокированный";
-    if ($row['role'] == 1) $role = "Учитель";
-    if ($row['role'] == 2) $role = "Завуч";
-    //для учителя
-if (isset($_POST['upd1'])) {
-    // Получаем данные из формы
-
-    $oldp = $_POST['password1'];
-    $algorithm = "sha256";
-    $oldp = hash($algorithm, $oldp);
-    $pas = $_POST['password2'];
-    $pas = hash($algorithm, $pas);
-    $checkpas = $_POST['password3'];
-    $checkpas = hash($algorithm, $checkpas);
-    if (($oldp == $pass) && ($pas = $checkpas))
-    {
-        $query = "UPDATE users SET passw = '$pas' WHERE id = $id";
-        $result = mysqli_query($connection, $query);
-        $res = 'Пароль успешно изменен!';
-    }
-    else $res = 'Пароли не совпадают, либо старый пароль не правильный!';
-
-}
-//для завучей
-    if (isset($_POST['upd2'])) {
-        // Получаем данные из формы
-
-        $newlogin = $_POST['login'];
-        $newrole = $_POST['role'];
-        $pas = $_POST['password2'];
-        $checkpas = $_POST['password3'];
-        if ((strlen($_POST['password2']) > 0) && ($pas = $checkpas))
-        {
-            $query = "UPDATE users SET passw = '$pas', login = '$newlogin', role = '$newrole' WHERE id = $id";
+        echo "<tr>";
+        echo "<th>ID</th><th>Логин</th><th>Роль</th><th>Редактировать</th>";
+        echo "</tr>";
+        for ($i = 1; $i <= $amount; $i++) {
+            $query = "SELECT * FROM users WHERE id = $i";
             $result = mysqli_query($connection, $query);
-            $log = $newlogin;
-            $role = $newrole;
-            $res = 'Изменения сохранены!';
+
+            if (mysqli_num_rows($result) == 1) {
+                $row = mysqli_fetch_assoc($result);
+                $id = $row['id'];
+                $login = $row['login'];
+                if ($row['role'] == 0) $role = "Заблокированный";
+                if ($row['role'] == 1) $role = "Учитель";
+                if ($row['role'] == 2) $role = "Завуч";
+
+            }
+            if (($_SESSION['role'] == 2) || ($_SESSION['username'] == $login)) {
+                echo "<tr>";
+                echo '<td><center>' . $id . '</center></td><td><center>' . $login . '</center></td><td><center>' . $role . '</center></td><td><center>' . '<a href="accedit.php?id=' . $id . '" class="button" >Настроить</a>' . '</center></td>';
+                echo "</tr>";
+            }
         }
-        else if (strlen($_POST['password2']) == 0)
-        {
-            $query = "UPDATE users SET login = '$newlogin', role = '$newrole' WHERE id = $id";
+        if (isset($_POST['submit'])) {
+            // Получаем данные из формы
+
+            $login = $_POST['login'];
+            $role = $_POST['role'];
+            $psw = $_POST['password'];
+            $query = "SELECT * FROM users WHERE login = '$login'";
             $result = mysqli_query($connection, $query);
-            $log = $newlogin;
-            $role = $newrole;
-            $res = 'Изменения сохранены!';
+
+            if (mysqli_num_rows($result) == 1) {
+                $res = "Пользователь с таким логином уже существует!";
+            }
+            else{
+                $algorithm = "sha256";
+                $psw = hash($algorithm, $psw);
+            $sql = "INSERT INTO users (login, passw, role) VALUES ('$login', '$psw', '$role')";
+            $result = mysqli_query($connection, $sql);
+            $res = "Аккаунт удачно зарегистрирован! Обновите страницу, чтобы увидеть его в таблице.";
+            header('Location: acctable.php');
+            }
         }
-        else
-
-            $res = 'Пароли не совпадают, либо старый пароль не правильный!';
-
-    }
-if (($_SESSION['role'] == 1) || ($_SESSION['role'] == 2)) {
-
-        if (($_SESSION['role'] == 1) && ($id == $_SESSION['id'])) {
-            if (isset($res)) echo $res;
-
-            echo '<form method="post" class="form-wrapper">';
-            echo "<center>";
-            echo '<h2>Редактирование аккаунта</h2>';
-            echo '<label>Логин: ' . $log . '</label><br>';
-           echo '<label>Роль: ' . $role . '</label><br>';
-
-            echo '<label>Старый пароль:</label><br> <input type="password" placeholder="Старый пароль" name="password1"><br>';
-            echo '<label>Новый пароль:</label><br> <input type="password" placeholder="Новый пароль" name="password2"><br>';
-            echo '<label>Повторите новый пароль:</label><br> <input type="password" placeholder="Новый пароль" name="password3"><br><br>';
-            echo '<input type="submit" name="upd1" value="Сменить">';
-            echo '</form>';
-        }
-        else if ($_SESSION['role'] == 2)
+        if ($_SESSION['role'] == 2)
         {
-            if (isset($res)) echo '<center>' .$res;
-            echo '<form method="post" class="form-wrapper">';
-            echo "<center>";
-            echo '<h2>Редактирование аккаунта</h2>';
-            echo '<label>Логин: </label><br><input type="text" class="rounded-input" name="login" value="' . $log .'"><br>';
-           // echo '<label>Роль: </label><input type="text" name="role" value="' . $row['role'] .'"><br>';
-            if ($role1 == 0)  echo '<label>Роль: </label><br><select name="role">
-    <option value="0">Заблокированный</option>
+        echo "</table>";
+        echo '<br><hr>';
+        echo '<title>Создание аккаунта</title>';
+        echo '<form method="post">';
+        echo '<h2>Создать аккаунт</h2>';
+        echo '<label>Логин:</label><br> <input type="text" class="rounded-input" placeholder="Логин" name="login"><br>';
+       // echo '<label>Роль:</label> <br><input type="text" placeholder="Роль" name="role"><br>';
+            ///
+          echo '<label>Роль:</label><br><select name="role">
     <option value="1">Учитель</option>
     <option value="2">Завуч</option>
-    
-</select><br>';
-
-            if ($role1 == 1) echo '<label>Роль: </label><br><select name="role">
-    <option value="1">Учитель</option>
     <option value="0">Заблокированный</option>
-    <option value="2">Завуч</option>
-    
 </select><br>';
-            if ($role1 == 2) echo '<label>Роль: </label><br><select name="role">
-    <option value="2">Завуч</option>
-    <option value="1">Учитель</option>
-    <option value="0">Заблокированный</option>
-    
-    
-</select><br>';
-
-            echo '<label>Новый пароль:</label> <br><input type="password" placeholder="Пароль" class="rounded-input" name="password2"><br>';
-            echo '<label>Повторите новый пароль:</label><br> <input type="password" placeholder="Пароль" class="rounded-input" name="password3"><br><br>';
-
-            echo '<input type="submit" name="upd2" value="Сохранить">';
-            echo '</form>';
+            ///
+        echo '<label>Пароль:</label><br> <input type="text" class="rounded-input" placeholder="Пароль" name="password"><br><br>';
+        echo '<input type="submit" name="submit" value="Создать!">';
+        echo '</form>';
+        if (isset($res)) {
+            echo '<p>' . $res . '</p>';
         }
-        else echo 'Отказано в доступе!';
     }
-else echo 'Отказано в доступе!';
-}	
+
+    }
+    else echo "Отказано в доступе";
